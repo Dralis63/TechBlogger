0c6f1d605a8b04855135dc1f27c0816e3da52306
Dralis63 <beleth63@gmail.com>
2024-09-03 09:53:47 +0200

    add config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const { series } = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-// Tâche pour compiler le SCSS en CSS
+// Tâche pour compiler le SCSS en CSS avec autoprefixer
 function compileSass() {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest('src/css'));
+}
+
+// Tâche pour optimiser les images
+async function optimizeImages() {
+    const imagemin = (await import('gulp-imagemin')).default;
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
 }
 
 // Tâche pour regarder les changements dans les fichiers SCSS
@@ -14,4 +24,10 @@ function watchSass() {
     gulp.watch('src/scss/**/*.scss', compileSass);
 }
 
-exports.default = series(compileSass, watchSass);
+// Export des tâches
+exports.compileSass = compileSass;
+exports.optimizeImages = optimizeImages;
+exports.watchSass = watchSass;
+
+// Tâche par défaut
+exports.default = gulp.series(compileSass, watchSass, optimizeImages);
